040729be2a5566ac68164803cf310aea567494dc
jose vitor <josevitorrodrigues18@gmail.com>
2025-05-08 19:03:32 -0300

    update dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/joho/godotenv"
+	"github.com/josevitorrodriguess/qrcode_generator/internal/api"
 	"github.com/josevitorrodriguess/qrcode_generator/internal/storage/awsS3"
 )
 
@@ -11,5 +14,11 @@ func main() {
 	s3 := awsS3.S3Storage{}
 
 	s3.InitSession()
-	
+	api := api.Api{
+		S3: s3,
+	}
+
+	http.HandleFunc("/qrcode", api.GenerateQrCodeHandler)
+
+	http.ListenAndServe(":3050", nil)
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-sdk-go v1.55.7 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 )
